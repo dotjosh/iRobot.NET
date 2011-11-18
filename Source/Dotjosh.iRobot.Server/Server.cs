945ea785be601911d8aca603dcb7f63f4854d3ae
Josh Schwartzberg <dotjosh@gmail.com>
2011-11-18 09:23:52 -0500

    Switching from OpenRasta to Nancy

--- Source/Dotjosh.iRobot.Server/Server.cs
+++ Source/Dotjosh.iRobot.Server/Server.cs
@@ -2,20 +2,13 @@ using System;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.IO;
-using System.Threading;
-using OpenRasta.Codecs;
-using OpenRasta.Configuration;
-using OpenRasta.DI;
-using OpenRasta.Hosting;
-using OpenRasta.Hosting.HttpListener;
-using OpenRasta.IO;
-using OpenRasta.Web;
+using Nancy.Hosting.Self;
 
 namespace Dotjosh.iRobot.Server
 {
 	public class Server
 	{
-		private AppDomainHost<ConfigurationExecutorHost> _http;
+		private NancyHost _nancyHost;
 
 		public void Start()
 		{
@@ -25,51 +18,18 @@ namespace Dotjosh.iRobot.Server
 			Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SqlCompactDataContext>());
 
 			int port = 6687;
-            _http = new AppDomainHost<ConfigurationExecutorHost>(new[] { string.Format("http://+:{0}/", port) }, "/",null);
-            _http.Initialize();
-			_http.Listener.Configure(() =>
-			{
-//			    ResourceSpace.Has.ResourcesOfType<Test>()
-//			        .AtUri("/test/{id}")
-//			        .HandledBy<CustomerHandler>()
-//			        .TranscodedBy<JsonDataContractCodec>(null);
+			_nancyHost = new NancyHost(new Uri("http://localhost:" + port), new Uri("http://127.0.0.1:" + port));
+			_nancyHost.Start();
 
-				ResourceSpace.Has
-					.ResourcesOfType<Stream>()
-					.AtUri("/{path}")
-					.HandledBy<EmbeddedResourceHandler>()
-					.TranscodedBy<ApplicationOctetStreamCodec>().ForMediaType("text/html").ForExtension(".html")
-					.And.TranscodedBy<ApplicationOctetStreamCodec>().ForMediaType("text/javascript").ForExtension(".js")
-					.And.TranscodedBy<ApplicationOctetStreamCodec>().ForMediaType("text/css").ForExtension(".css")
-					.And.TranscodedBy<ApplicationOctetStreamCodec>().ForMediaType("image/png").ForExtension(".png");
-			});
-			_http.StartListening();
-		}
+			}
 
 		public void Stop()
 		{
 			Console.WriteLine("End");
-			if (_http == null) 
+			if (_nancyHost == null) 
 				return;
-			_http.StopListening();
-			_http = null;
-		}
-	}
-
-	public class ConfigurationExecutorHost : HttpListenerHost
-	{
-		private Action t;
-
-		public void Configure(Action t)
-		{
-			this.t = t;
-		}
-
-		public override bool ConfigureLeafDependencies(IDependencyResolver resolver)
-		{
-			using (OpenRastaConfiguration.Manual)
-				t();
-			return true;
+			_nancyHost.Stop();
+			_nancyHost = null;
 		}
 	}
 }
