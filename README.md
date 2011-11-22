a2374fd60ae982604645092cd3669c4aab86764c
Josh Schwartzberg <dotjosh@gmail.com>
2011-11-22 00:18:34 -0500

    readme tweak

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Summary
 
 Controlling your iRobot Create should be fun, sleek, and accessible from all of your devices that have a web browser!
 
-Just mount a laptop with a webcam on your iRobot Create, connect it to the serial port, install this project as a windows service (installutil.exe /i dotjosh.irobot.server.exe) and connect to the web ui that's hosted in-process on port 6687 (e.g. http://yourlaptop:6687).
+Just mount a laptop with a webcam on your iRobot Create, connect it to the serial port, install this project as a windows service (from a command line run: dotjosh.irobot.server.exe install) and connect to the web ui that's hosted in-process on port 6687 (e.g. http://yourlaptop:6687).
 
 * Full keyboard driving support (like a video game) in the browser, just use your arrow keys.
 * Clean, modern, 100% HTML/JS UI built right in or you can use the exposed JSON API and make your own!
--- Source/Dotjosh.iRobot.Server/Program.cs
+++ Source/Dotjosh.iRobot.Server/Program.cs
@@ -4,23 +4,26 @@ namespace Dotjosh.iRobot.Server
 {
 	class Program
 	{
-		static void Main()
+		static void Main(string[] args)
 		{
-			HostFactory.Run(x =>
+			var h = HostFactory.New(x =>
 			{
-				x.SetDescription("iRobot.NET Web host and and Serial Port communicator");
-				x.SetDisplayName("iRobot.NET Server");
-				const string serviceName = "irobotdotnet";
-				x.SetServiceName(serviceName);
-
+				const string serviceName = "irobot";
 				x.Service<WebServer>(c =>
 				{
-					c.SetServiceName(serviceName);
+					c.SetServiceName("WebServer");
 					c.ConstructUsing(() => new WebServer());
 					c.WhenStarted(d => d.Start());
 					c.WhenStopped(d => d.Stop());
 				});
+
+				x.RunAsLocalSystem();
+				x.SetDescription("iRobot.NET Web host and and Serial Port communicator");
+				x.SetDisplayName("iRobot.NET Server");
+				x.SetServiceName(serviceName);
 			});
+
+			h.Run();
 		}
 	}
 }
